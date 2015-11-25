c04c4725442e1497a4086b52b699234dd7141c0e
Mark <marktraquair@gmail.com>
2015-11-24 19:27:20 -0500

    Tournament creator created Extremely minor code addition to teamcreator

---
+++ app/src/main/java/shakirateam/tournamentmaker/TournamentCreator.java
@@ -0,0 +1,17 @@
+package shakirateam.tournamentmaker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Mark on 2015-11-24.
+ */
+public class TournamentCreator extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.tournamentcreator);
+    }
+}
