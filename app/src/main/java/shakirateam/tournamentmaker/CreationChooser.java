a7504efd524f47b34be6dcbb8b8c1d7ebe0d7331
Mark <marktraquair@gmail.com>
2015-12-03 18:24:26 -0500

    Teams re-instated Changes to teamcreator and creationchooser

--- app/src/main/java/shakirateam/tournamentmaker/CreationChooser.java
+++ app/src/main/java/shakirateam/tournamentmaker/CreationChooser.java
@@ -1,8 +1,11 @@
 package shakirateam.tournamentmaker;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Mark on 2015-11-24.
@@ -22,5 +25,22 @@ public class CreationChooser extends Activity {
         int height = dm.heightPixels;
 
         getWindow().setLayout(width-20, height/4);
+
+        Button team = (Button) findViewById(R.id.team_creation_button);
+        Button tournament = (Button) findViewById(R.id.tournament_creation_button);
+
+        team.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CreationChooser.this, teamcreator.class));
+            }
+        });
+
+        tournament.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CreationChooser.this, TournamentCreator.class));
+            }
+        });
     }
 }
--- app/src/main/java/shakirateam/tournamentmaker/teamcreator.java
+++ app/src/main/java/shakirateam/tournamentmaker/teamcreator.java
@@ -1,5 +1,6 @@
 package shakirateam.tournamentmaker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -19,6 +20,14 @@ public class teamcreator extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         Button confirm = (Button) findViewById(R.id.confirm_button);
+        Button img = (Button) findViewById(R.id.image_button);
+
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(teamcreator.this, Teams.class));
+            }
+        });
     }
 
 }
